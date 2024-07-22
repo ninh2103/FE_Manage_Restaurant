2874e8673b3fadb585cf51e1b0221e617d407b76
Nguyen Duc Ninh <nguyenducninh210303@gmail.com>
2024-07-22 22:11:52 +0700

    22/7

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -7,24 +7,46 @@ import { getTableLink } from "@/lib/utils";
 export default function QRCodetable({
   token,
   tableNumber,
+  width = 250,
 }: {
   token: string;
   tableNumber: number;
+  width?: number;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-    const canvas = canvasRef.current;
+    const canvas = canvasRef.current!;
+    canvas.height = width + 70;
+    canvas.width = width;
+
+    const canvasContext = canvas.getContext("2d")!;
+    canvasContext.fillStyle = "#fff";
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContext.font = "20px Arial";
+    canvasContext.textAlign = "center";
+    canvasContext.fillStyle = "#000";
+    canvasContext.fillText(
+      `Bàn Số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    );
+    canvasContext.fillText(
+      `Quét Mã QR Để Gọi Món`,
+      canvas.width / 2,
+      canvas.width + 50
+    );
+    const virtalCanvas = document.createElement("canvas");
     QRCode.toCanvas(
-      canvas,
+      virtalCanvas,
       getTableLink({
         token,
         tableNumber,
       }),
       function (error) {
         if (error) console.log(error);
-        console.log("success");
+        canvasContext.drawImage(virtalCanvas, 0, 0, width, width);
       }
     );
-  }, [token, tableNumber]);
+  }, [token, width, tableNumber]);
   return <canvas ref={canvasRef} />;
 }
